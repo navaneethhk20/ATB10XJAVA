90e7b94a609bc554352beb3971dbdc3b2fc2aa63
navaneethhhk <hknavaneeth@gmail.com>
2025-01-19 20:29:38 +0530

    table of nine

---
+++ src/Java_Basics/Lab010_PrintTableOf9.java
@@ -0,0 +1,18 @@
+package Java_Basics;
+
+public class Lab010_PrintTableOf9 {
+    public static void main(String[] args) {
+        int t =9;
+        System.out.printf("%d * 1 =%d\n", t, t);
+        System.out.printf("%d * 2 =%d\n", t, t);
+        System.out.printf("%d * 3 =%d\n", t, t);
+        System.out.printf("%d * 4 =%d\n", t, t);
+        System.out.printf("%d * 5 =%d\n", t, t);
+        System.out.printf("%d * 6 =%d\n", t, t);
+        System.out.printf("%d * 7 =%d\n", t, t);
+        System.out.printf("%d * 8 =%d\n", t, t);
+        System.out.printf("%d * 9 =%d\n", t, t);
+        System.out.printf("%d * 10 =%d\n", t, t);
+
+    }
+}
