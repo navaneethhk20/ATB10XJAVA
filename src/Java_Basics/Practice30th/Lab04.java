e368aa525bafcf3717055c5547ea237f043d9987
navaneethhhk <hknavaneeth@gmail.com>
2025-02-06 09:26:20 +0530

    Today06022025-part1

--- src/Java_Basics/Practice30th/Lab04.java
+++ src/Java_Basics/Practice30th/Lab04.java
@@ -1,8 +1,18 @@
 package Java_Basics.Practice30th;
 
+import java.util.Scanner;
+
 public class Lab04 {
     public static void main(String[] args) {
         //Check if a Year is a Leap Year.
+Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year");
+        int year = sc.nextInt();
 
+        if(year %4 == 0 && year%100 !=0 || year%400 ==0){
+            System.out.println("Leap year");
+        }else {
+            System.out.println("Not a leap year");
+        }
     }
 }
---
+++ src/Java_Basics/Practice30th/Lab05.java
@@ -0,0 +1,20 @@
+package Java_Basics.Practice30th;
+
+public class Lab05 {
+    public static void main(String[] args) {
+//        Calculate Grade Based on Marks. (Follow Below Steps to Write Program.)
+//        1️⃣ Take user input for marks (Use Scanner class).
+//        2️⃣ Check the validity of marks (ensure they are between 0 and 100).
+//        3️⃣ Use if-else-if conditions to determine the grade based on marks.
+//        4️⃣ Display the grade as output.
+//📌 Grading Criteria (Example)
+//      Marks Range Grade
+//        90 - 100   A+
+//        80 - 89     A
+//        70 - 79      B
+//        60 - 69     C
+//        50 - 59      D
+//        40 - 49      E
+//        Below 40   Fail
+    }
+}
